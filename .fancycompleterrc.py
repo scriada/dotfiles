ac3b6906cc144040984a04fe368eaf1644b9f3d3
Adam Scriven <adam.scriven+github@gmail.com>
2017-04-23 17:57:26 +0100

    pdb++ configuration

---
+++ .fancycompleterrc.py
@@ -0,0 +1,30 @@
+import os
+import atexit
+from fancycompleter import DefaultConfig
+ 
+
+class Config(DefaultConfig):
+
+    prefer_pyrepl = False # force fancycompleter to use the standard readline
+ 
+    def setup(self):
+        DefaultConfig.setup(self)
+ 
+        _setup_readline(self.readline)
+ 
+ 
+def _setup_readline(readline):
+ 
+    histfile = os.path.expanduser("~/.fancy-hist")
+ 
+    readline.parse_and_bind("\C-k: history-search-backward")
+    readline.parse_and_bind("\C-j: history-search-forward")
+ 
+    try:
+        readline.read_history_file(histfile)
+    except IOError:
+        pass
+ 
+    readline.set_history_length(500)
+ 
+    atexit.register(readline.write_history_file, histfile)
---
+++ .pdbrc.py
@@ -0,0 +1,14 @@
+from pdb import DefaultConfig
+
+class Config(DefaultConfig):
+    """
+    Configuration for pdb++
+    See .fancycompleterc.py for readline setup
+    """
+
+    prompt = 'pdb> '
+    encoding = 'ascii'
+    current_line_color = 7
+    sticky_by_default = True
+
+
