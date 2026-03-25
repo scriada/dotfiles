abdce3c0dae281a589cfbe7e7abfe26cdea34111
Adam Scriven <adam.scriven+github@gmail.com>
2026-03-25 21:57:57 +0000

    nvim ide settings

--- .config/nvim/lua/init.lua
+++ .config/nvim/lua/init.lua
@@ -53,6 +53,7 @@ vim.api.nvim_create_autocmd('LspAttach', {
     local opts = { buffer = ev.buf }
     vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
     vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
+    vim.keymap.set('n', 'dq', vim.diagnostic.setloclist)
     vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
     vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
     vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, opts)
@@ -72,6 +73,16 @@ vim.api.nvim_create_autocmd('LspAttach', {
 
     -- setup signature_lsp
     require("lsp_signature").on_attach({}, ev.buf)
+    require("aerial").setup({
+      -- optionally use on_attach to set keymaps when aerial has attached to a buffer
+      on_attach = function(bufnr)
+        -- Jump forwards/backwards with '{' and '}'
+        vim.keymap.set("n", "{", "<cmd>AerialPrev<CR>", { buffer = bufnr })
+        vim.keymap.set("n", "}", "<cmd>AerialNext<CR>", { buffer = bufnr })
+      end,
+    })
+    vim.keymap.set("n", "<leader>a", "<cmd>AerialToggle!<CR>")
+    vim.keymap.set("n", "<f7>", "<cmd>AerialToggle left<CR>")
   end,
 })
 
